24690e9ed6bfbe98dace1381f1ef93f370ddf71a
SimoL-003 <simoneleuzzi2003@gmail.com>
2025-09-18 18:56:56 +0200

    feat: code improvement

--- js/script.js
+++ js/script.js
@@ -17,16 +17,15 @@ ESECUZIONE LOGICA
 */
 
 for (let i = 1; i < 101; i++) {
+    let result = i;
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
+        result = "FizzBuzz";
     }
     else if (i % 3 === 0) {
-        console.log("Fizz");
+        result = "Fizz";
     }
     else if (i % 5 === 0) {
-        console.log("Buzz");
+        result = "Buzz";
     }
-    else {
-        console.log(i);
-    }
+    console.log(result);
 }
